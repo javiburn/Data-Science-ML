7d10ae21027813ab9c481aafb94af3bc25371f23
Javier Sarabia Rodriguez <jsarabia@c3r12s1.42madrid.com>
2024-07-03 17:10:59 +0200

    creating Source file

---
+++ 02_Análisis de Medallas Olímpicas/main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from model.source import Source
+
+def main():
+    source = Source("./medallas.csv")
+
+if __name__ == '__main__':
+    main()
---
+++ 02_Análisis de Medallas Olímpicas/model/source.py
@@ -0,0 +1,9 @@
+class Source:
+    def __init__(self, path):
+        self.path  = path
+        self.open_file()
+
+    def open_file(): # TODO
+        try:
+            self.file = open
+        except:
