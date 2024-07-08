6cc13e86f4e8e8631ef3a48da50ea049823eb4c8
Javier Sarabia Rodriguez <jsarabia@c3r12s1.42madrid.com>
2024-07-08 20:16:12 +0200

    obtaining the main() temperature

--- 04_Análisis de Datos Meteorológicos/main.py
+++ 04_Análisis de Datos Meteorológicos/main.py
@@ -10,10 +10,18 @@ def load_csv():
     return df
 
 def main():
-    #df = load_csv()
-    array = np.genfromtxt("datos_meteorologicos.csv", delimiter=',', \
-        filling_values=0, skip_header=1)
-    print(array)
+    df = load_csv()
+    # We convert the pd dataframe to a numpy array
+    array_temp = df['Temperatura'].to_numpy()
+    array_prec = df['Precipitación'].to_numpy()
+    array_hum = df['Humedad'].to_numpy()
+
+    # We substitute the nan values with the mean() value of the other rows ignoring the other nan values too
+    temp_nulo = np.isnan(array_temp)
+    temp_mean = np.nanmean(array_temp)
+    array_temp = np.where(temp_nulo, temp_mean, array_temp)
+    print("Temperatura promedio: ")
+    print(temp_mean)
 
 if __name__ == '__main__':
     main()
