19815467ed33af1a4e983f5cdcf27d47008b2ebf
Javier Sarabia Rodriguez <jsarabia@c3r12s1.42madrid.com>
2024-07-09 12:18:19 +0200

    finished task

--- 04_Análisis de Datos Meteorológicos/main.py
+++ 04_Análisis de Datos Meteorológicos/main.py
@@ -9,6 +9,33 @@ def load_csv():
         exit(1)
     return df
 
+def show_mean_temp(array_temp):
+    # We substitute the nan values with the mean() value of the other rows ignoring the other nan values too
+    temp_mean = np.nanmean(array_temp)
+    temp_nulo = np.isnan(array_temp)
+    array_temp = np.where(temp_nulo, temp_mean, array_temp)
+    print("Temperatura promedio: ")
+    print(temp_mean)
+    return array_temp
+
+def show_total_prec(array_prec):
+    prec_nulo = np.isnan(array_prec)
+    prec_mean = np.nanmean(array_prec)
+    array_prec = np.where(prec_nulo, prec_mean, array_prec)
+    prec_total = np.nansum(array_prec)
+    print("Total precipitaciones:")
+    print(prec_total)
+    return array_prec
+
+def show_max_hum(array_hum):
+    hum_nulo = np.isnan(array_hum)
+    hum_mean = np.nanmean(array_hum)
+    array_hum = np.where(hum_nulo, hum_mean, array_hum)
+    hum_max = np.max(array_hum)
+    print("Humedad máxima:")
+    print(hum_max)
+    return array_hum
+
 def main():
     df = load_csv()
     # We convert the pd dataframe to a numpy array
@@ -16,12 +43,23 @@ def main():
     array_prec = df['Precipitación'].to_numpy()
     array_hum = df['Humedad'].to_numpy()
 
-    # We substitute the nan values with the mean() value of the other rows ignoring the other nan values too
-    temp_nulo = np.isnan(array_temp)
-    temp_mean = np.nanmean(array_temp)
-    array_temp = np.where(temp_nulo, temp_mean, array_temp)
-    print("Temperatura promedio: ")
-    print(temp_mean)
+    # Clean the data converting NaN values into the mean() values
+    clean_temp = show_mean_temp(array_temp)
+    clean_prec = show_total_prec(array_prec)
+    clean_hum = show_max_hum(array_hum)
+
+    # Create the new and cleant DataFrame
+    df['Temperatura'] = clean_temp
+    df['Precipitacion'] = clean_prec
+    df['Humedad'] = clean_hum
+
+    # Print hottest and coldest day
+    hot = df.sort_values(by='Temperatura', ascending=False).head(1)
+    print("Día más caluroso:")
+    print(hot['Fecha'].to_string(index=False))
+    cold = df.sort_values(by='Temperatura').head(1)
+    print("Día más frío:")
+    print(cold['Fecha'].to_string(index=False))
 
 if __name__ == '__main__':
     main()
